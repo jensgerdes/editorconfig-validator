3b6a776305c88facb8f01266b45b9111cab13574
Jens Gerdes <jgerdes@b1n4ry.eu>
2017-12-06 20:22:33 +0100

    Added .editorconfig

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LibraryTest {
-    @Test
-    void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
-    }
+	@Test
+	void testSomeLibraryMethod() {
+		Library classUnderTest = new Library();
+		assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+	}
 }
